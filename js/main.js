a34e0f6284270c40b35c403804892e7470ff5344
Neksov <n2v@list.ru>
2020-05-11 18:39:40 +0300

    new work

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
   loop: true,
   autoHeight: true,
   spaceBetween: 100,
-  //autoplay: {
-  // delay: 3000,
-  //},
+  autoplay: {
+    delay: 3000,
+  },
+})
+new WOW().init();
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
 })
